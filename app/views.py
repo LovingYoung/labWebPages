90804eb2cafcce6256f71c96118d40b10ad43ae7
LovingYoung <ly950710@gmail.com>
2016-05-26 21:13:52 +0800

    create Posts

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from flask import render_template, request, redirect, flash
 from app import app, forms, db, models, login_manager
 from flask.ext.login import current_user, login_required, login_user, logout_user
 import hashlib
+import datetime
 
 @app.route('/')
 @app.route('/index')
@@ -60,3 +61,42 @@ def logout():
     logout_user()
     flash("Log out Success")
     return redirect('index')
+
+@app.route('/manage')
+@login_required
+def manage():
+    titleArgs = request.args.get('title')
+    typeArgs = request.args.get('type')
+    data = None
+    if titleArgs is None and typeArgs is None:
+        data = models.Post.query.all()
+    elif titleArgs is None:
+        data = models.Post.query.filter_by(type=typeArgs).all()
+    elif typeArgs is None:
+        data = models.Post.query.filter_by(title=titleArgs).all()
+    else:
+        data = models.Post.query.filter_by(type=typeArgs, title=titleArgs).all()
+    render_template("manage.html", data=data)
+    #TODO: do manage.html
+
+@app.route('/modify/<int:postid>', methods=['GET', 'POST'])
+@login_required
+def modify(postid):
+    post = models.Post.query.filter_by(postid=postid).first()
+    if post is None:
+        flash("Can't find Post with ID " + str(postid))
+        redirect("manage")
+    render_template("modify.html", title=post.title, content=post.content, type=post.type)
+    #TODO: do modify.html
+
+@app.route('/create', methods=['POST', 'GET'])
+@login_required
+def create():
+    form = forms.PostForm(request.form)
+    if request.method == 'GET':
+        return render_template('create.html', form = form)
+    if form.validate():
+        post = models.Post(userid=current_user.id, content=form.content.data, title=form.title.data, type=form.type.data, createdTime=datetime.datetime.now(), modifiedTime=datetime.datetime.now())
+        db.session.add(post)
+        db.session.commit()
+        return redirect('index')
