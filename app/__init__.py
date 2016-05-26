f5924cbd5889dd07a53a0fdbcf848a250525908e
LovingYoung <ly950710@gmail.com>
2016-05-26 12:11:03 +0800

    Initial Flask

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+import pymysql
+from flask.ext.login import LoginManager
+
+
+#initial
+pymysql.install_as_MySQLdb()
+login_manager = LoginManager()
+
+app = Flask(__name__)
+app.config.from_object('config')
+login_manager.init_app(app)
+db = SQLAlchemy(app)
+
+from app import views, models
